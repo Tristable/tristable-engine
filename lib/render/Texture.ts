443355fd996e46160c7fa9e249ffee810bcb67a7
TrueSunGaming <truesungaming@gmail.com>
2023-05-03 21:50:27 -0400

    Texture now uses ImageBitmap not CanvasImageSource

--- lib/render/Texture.ts
+++ lib/render/Texture.ts
@@ -1,26 +1,30 @@
-import { Rect2, Rectangle } from "../math/Rect2.js";
+import { Rect2 } from "../math/Rect2.js";
 import { Point, Vector2 } from "../math/Vector2.js";
 import { TristableError } from "../core/TristableError.js";
 
 /** A texture that can be rendered. */
 export class Texture {
-    /** The `CanvasImageSource` for the `Texture` to use. */
-    src: CanvasImageSource;
+    /** The `ImageBitmap` for the `Texture` to use. */
+    src: ImageBitmap;
     
-    /** The portion of the `CanvasImageSource` for the `Texture` to use. Usually used for getting a single texture from a spritesheet. */
+    /** The portion of the `ImageBitmap` for the `Texture` to use. Usually used for getting a single texture from a spritesheet. */
     partition?: Rect2;
 
-    constructor(src: CanvasImageSource, partition?: Rect2) {
+    constructor(src: ImageBitmap, partition?: Rect2) {
         this.src = src;
         this.partition = partition;
     }
 
+    static async createFromSource(source: ImageBitmapSource, partition?: Rect2, fallback?: Texture): Promise<Texture> {
+        return new Texture(await (fallback != undefined ? createImageBitmap(source).catch(() => fallback.src) : createImageBitmap(source)), partition);
+    }
+
     /** Loads a single texture from a URL or file path. */
     static loadFromURL(url: string, partition?: Rect2, fallback?: Texture): Promise<Texture> {
-        return new Promise((r: (v: Texture) => void, rj: (r: ErrorEvent) => void) => {
-            const src = new Image();
+        return new Promise((r: (v: Texture) => void, rj: (r: any) => void) => {
+            const src: HTMLImageElement = new Image();
             src.src = url;
-            src.addEventListener("load", () => r(new Texture(src, partition)));
+            src.addEventListener("load", () => Texture.createFromSource(src, partition, fallback).then(r).catch(rj));
             src.addEventListener("error", (e: ErrorEvent) => {
                 if (!fallback) {
                     rj(e);
@@ -33,7 +37,7 @@ export class Texture {
     }
 
     /** Repeats `source` across a texture of size `size` every `repeatSize` units. `source` is scaled to `repeatSize`. */
-    static createTiled(source: Texture, size: Vector2, repeatSize: Vector2): Texture {
+    static createTiled(source: Texture, size: Vector2, repeatSize: Vector2): Promise<Texture> {
         const oc: OffscreenCanvas = new OffscreenCanvas(size.x, size.y);
         const c: OffscreenCanvasRenderingContext2D = oc.getContext("2d")!;
 
@@ -51,18 +55,19 @@ export class Texture {
             }
         }
 
-        return new Texture(oc);
+        return Texture.createFromSource(oc);
     }
 
     /** Loads multiple textures from a single source URL or file path. */
     static loadSpritesheetFromURL(url: string, partitions: Map<string, Rect2>, fallback?: Texture): Promise<Map<string, Texture>> {
-        return new Promise((r: (v: Map<string, Texture>) => void, rj: (r: ErrorEvent) => void) => {
+        return new Promise((r: (v: Map<string, Texture>) => void, rj: (r: any) => void) => {
             const src = new Image();
             src.src = url;
             src.addEventListener("load", () => {
                 const textures: Map<string, Texture> = new Map();
-                for (const [k, v] of partitions) textures.set(k, new Texture(src, v));
-                r(textures);
+                const promises: Promise<void>[] = [];
+                for (const [k, v] of partitions) promises.push((Texture.createFromSource(src, v, fallback).then((t) => textures.set(k, t), undefined) as Promise<void>).catch(rj));
+                Promise.all(promises).then(() => r(textures));
             });
             src.addEventListener("error", (e: ErrorEvent) => {
                 if (!fallback) {
@@ -85,16 +90,16 @@ export class Texture {
     }
 
     /** Creates an empty bitmap `Texture` with a certain size. */
-    static generateEmpty(size: Point): Texture {
-        return new Texture(new OffscreenCanvas(size.x, size.y));
+    static async generateEmpty(size: Point): Promise<Texture> {
+        return await Texture.createFromSource(new OffscreenCanvas(size.x, size.y));
     }
 
     /** Creates a solid color bitmap `Texture` with a certain size and color. */
-    static solidColor(size: Point, color: string): Texture {
+    static async solidColor(size: Point, color: string): Promise<Texture> {
         const ca: OffscreenCanvas = new OffscreenCanvas(size.x, size.y);
         const c: OffscreenCanvasRenderingContext2D = ca.getContext("2d")!;
         c.fillStyle = color;
         c.fillRect(0, 0, size.x, size.y);
-        return new Texture(ca);
+        return Texture.createFromSource(ca);
     }
 }
